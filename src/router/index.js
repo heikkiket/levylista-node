001b8e624dce6542b9b4a151e1493c299d9334ad
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-02-16 23:45:53 +0200

    Added router

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,27 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Albums from '../views/Albums';
+import SingleAlbum from '../views/SingleAlbum';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/albums',
+    name: 'Albums',
+    component: Albums
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/albums/single',
+    name: 'Single album',
+    component: SingleAlbum
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
