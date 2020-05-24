b39f6715a94bb4bf8d33c3a3de04f14a3a224f1e
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-05-24 21:19:54 +0300

    New album component and grid system for them

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,69 @@
+<template lang="html">
+    <div class="album-box">
+        <router-link :to="albumLink">
+            <div class="album-content">
+                <h2>{{album.artist}}</h2>
+                <h1>{{album.album}}</h1>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AlbumCard',
+    props: {
+        album: {
+            Type: Object
+        }
+    },
+    data () {
+        return {
+        }
+    },
+    watch: {
+    },
+    computed: {
+        albumLink() {
+            return "/albums/single/" + this.album._id;
+        },
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+
+ a {
+ }
+
+ .album-box {
+     background: #dcc;
+     border-radius: 10px;
+     width: 12rem;
+     height: 12rem;
+     padding: 0.5rem;
+ }
+
+ .album-content {
+     display: grid;
+     grid-template-rows: 3rem 6rem 3rem;
+ }
+
+ h1, h2 {
+     margin: 0;
+     word-break: break-word;
+ }
+
+ h1 {
+     grid-row: 1 / span 1;
+     font-size: 1.5rem;
+ }
+
+ h2 {
+     grid-row: 3 / span 1;
+     font-size: 1.3rem;
+     text-align: bottom;
+ }
+</style>
--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -3,9 +3,7 @@
         <h1>Albums</h1>
         <ul>
             <li v-for="(album, index) in albums" :key="index">
-                <router-link :to="'/albums/single/' + album._id">
-                    {{album.artist}}: {{album.album}}
-                </router-link>
+                <AlbumCard :album="album" />
             </li>
         </ul>
     </div>
@@ -13,10 +11,14 @@
 
 <script>
 import { mapActions } from 'vuex';
+import  AlbumCard  from '../components/AlbumCard.vue';
 export default {
     name: 'Albums',
     props: {
         msg: String
+    },
+    components: {
+        AlbumCard
     },
     data() {
         return {
@@ -31,9 +33,6 @@ export default {
         }
     },
     methods: {
-        albumLink(id) {
-            return "/albums/single/" + id;
-        },
         ...mapActions([
             'getAlbums',
         ]),
@@ -46,7 +45,47 @@ export default {
  h3 {
      margin: 40px 0 0;
  }
+
  a {
      color: #42b983;
+ }
+
+ .albums {
+     display: block;
+ }
+
+ ul {
+     display: grid;
+     grid-template-columns: repeat(auto-fill, 13rem);
+     grid-gap: 1rem;
+     align-items: center;
+     margin: 0 auto;
+     padding: 0;
+ }
+
+ li {
+     list-style-type: none;
+ }
+
+ ul {
+     width: 56rem;
+ }
+
+ @media screen and (max-width: 768px) {
+     ul {
+         width: 41rem;
+     }
+ }
+
+ @media screen and (max-width: 670px) {
+     ul {
+         width: 27rem;
+     }
+ }
+
+ @media screen and (max-width: 450px) {
+     ul {
+         width: 13rem;
+     }
  }
 </style>
